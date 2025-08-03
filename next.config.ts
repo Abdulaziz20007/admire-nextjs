22d8ed33cbcd40dd92b6ede86fe73ea24ee42266
Abdulaziz <lil.gangsser@gmail.com>
2025-08-03 11:16:42 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.admirelc.uz",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
